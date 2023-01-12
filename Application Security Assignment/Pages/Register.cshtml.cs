adeed983927781987d030bc2e62966f9d06576c7
Declan <declanisaackuok@gmail.com>
2023-01-12 13:27:44 +0800

    Added register function

--- Application Security Assignment/Pages/Register.cshtml.cs
+++ Application Security Assignment/Pages/Register.cshtml.cs
@@ -3,6 +3,9 @@ using Application_Security_Assignment.UiState;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.FileSystemGlobbing.Internal;
+using System.Text.RegularExpressions;
+
 
 namespace Application_Security_Assignment.Pages
 {
@@ -21,16 +24,48 @@ namespace Application_Security_Assignment.Pages
 
         public void OnGet()
         {
+
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
-            var newUser = new ApplicationUser()
+            if(ModelState.IsValid)
             {
+                if(!(new Regex("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\\s).{12}").IsMatch(RegisterUiState.Password)))
+                {
+                    ModelState.AddModelError("RegisterUiState.Password", "Your password needs to be strong!");
+                    return Page();
+                }
 
-            };
+                var newUser = new ApplicationUser()
+                {
+                    Email = RegisterUiState.Email,
+                    FullName = RegisterUiState.FullName,
+                    CreditCardNo = RegisterUiState.CreditCardNo,
+                    Gender = RegisterUiState.gender,
+                    MobileNo = RegisterUiState.MobileNo,
+                    DeliveryAddress = RegisterUiState.DeliveryAddress,
+                    //imageurl later
+                    UserName = RegisterUiState.FullName,
+                    AboutMe = RegisterUiState.AboutMe,
+                };
+                var result = await _userManager.CreateAsync(newUser, RegisterUiState.Password);
+                if(result.Succeeded)
+                {
+                    return Redirect("/login");
+                } else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    return  Page();
+                }
+              
+            }
 
-            var result = await _userManager.CreateAsync(newUser, Password);
+
+
             return Page();
         }
     }
--- Application Security Assignment/UiState/RegisterUiState.cs
+++ Application Security Assignment/UiState/RegisterUiState.cs
@@ -5,27 +5,52 @@ namespace Application_Security_Assignment.UiState
 {
     public class RegisterUiState
     {
+        public Gender gender;
         [Required]
+        [Display(Name ="Full Name")]
+   
         public string? FullName { get; set; }
 
+        [Display(Name = "Credit Card Number")]
+        [DataType(DataType.CreditCard)]
         public string? CreditCardNo { get; set; }
 
         [Required]
-        public Gender? Gender { get; set; }
-
+        public String? Gender {
+            get => gender.ToString();
+            set
+            {
+                gender = (Gender)Enum.Parse(typeof(Gender), value);
+            }
+        }
         [Required]
+        [DataType(DataType.PhoneNumber)]
         public string? MobileNo { get; set; }
         [Required]
         public string? DeliveryAddress { get; set; }
 
-        public string? ImageURL { get; set; }
 
+        public IFormFile? Image { get; set; }
+
+
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "About me")]
         public string? AboutMe { get; set; }
 
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Required]
+        [Compare(nameof(Password), ErrorMessage = "Confirm password does not match the password")]
         public string? ConfirmPassword { get; set; }
 
+        [DataType(DataType.Password)]
+        [Required]
         public string? Password { get; set; }
 
+        [DataType(DataType.EmailAddress)]
+        [Required]
         public string? Email { get; set; }
+
+     
     }
 }
